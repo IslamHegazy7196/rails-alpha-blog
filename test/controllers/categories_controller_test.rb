5f97df7a64d4eb67269ac4f656946654561710a2
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2022-12-10 11:51:27 +0200

    fix: monkey patch functional test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -16,7 +16,7 @@ class CategoriesControllerTest < ActionController::TestCase
     end
 
     test "should get show" do
-        get (:show,{'id'=>@category.id})
+        get :show
         assert_response :success
     end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,29 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    p "44444444444444"
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      p "44444444444444"
+      def recycle!
+        p "23456987654"
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
+
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
 end
