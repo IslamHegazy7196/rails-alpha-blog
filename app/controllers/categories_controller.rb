ef5f3e83fedeba64d06fe3a89323354a8e57ca1a
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2022-12-12 22:51:57 +0200

    feat: addintegration test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,36 @@
 class CategoriesController < ApplicationController
-    def new
+    before_action :require_admin, except: [:index, :show]
 
+    def new
+        @category=Category.new
+    end
+
+    def create
+       @category=Category.new(category_params)
+       if @category.save
+        flash[:success]="Category was created successfully"
+        redirect_to categories_path
+       else
+        render 'new'
+       end
     end
 
     def index
-
+        @categories=Category.paginate(page: params[:page], per_page:5)
     end
 
     def show
         
+    end
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !logged_in? || (logged_in? and !current_user.admin?)
+            flash[:danger]="Only Admins can perform this action"
+            redirect_to categories_path
+        end
     end
 end
